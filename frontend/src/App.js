0e73525bfb6bc1352c3a2567bcbce1e2baa1771e
vishal-9696 <106727353+vishal-9696@users.noreply.github.com>
2023-07-31 22:02:21 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+
+import {Route} from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
+import './App.css';
+
+ import React from 'react'
+ 
+  
+ 
+
+
+function App() {
+  return (
+    <div className="App">
+     <Route path="/" component={HomePage }exact/>
+     <Route path="/chats" component={ChatPage}/>
+      
+    </div>
+  );
+}
+
+export default App;
